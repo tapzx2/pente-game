9f09b108569e5e7a94047b16f313e45ddc41a4da
Nicholas Tapia <tapia.nicholas@gmail.com>
2020-05-30 14:21:11 -0700

    print CSS for grid logic

--- resources/gridGenerator.js
+++ resources/gridGenerator.js
@@ -1,16 +1,8 @@
-var grid = 3;
+var grid = 5;
 
+printGrid(grid);
+printGridCSS(grid);
 
-var gridContainerOpen = '<div class="grid-container">\n'
-var gridContainerClose = '</div>';
-
-let output = '';
-for (let i = 1; i <= gridItems; i++){
-  output+= `<div class="grid-item">${i}</div>\n`
-}
-console.log(output);
-
-console.log(gridContainerOpen + gridContainerClose);
 
 function printGrid(grid){
   let output = '';
@@ -18,6 +10,18 @@ function printGrid(grid){
   output += '<div class="grid-container">\n'
   for (let i = 1; i <= (grid*grid); i++){
     output+= `  <div class="grid-item">${i}</div>\n`
+  }
   output += gridContainerClose = '</div>';
+  console.log(output);
+};
+
+
+function printGridCSS(grid){
+  let output = '';
+  output += `.grid-container {\n`;
+  output += `  display: grid;\n`
+  output += `  grid-template-columns: repeat(${grid}, 50px);\n`;
+  output += `  grid-template-rows: repeat(${grid}, 50px);\n`;
+  output += `}`
   console.log(output);
 }
