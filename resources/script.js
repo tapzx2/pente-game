8a640ab6dddfaf311e84f9d16ba1bae0751d0c4e
Nicholas Tapia <tapia.nicholas@gmail.com>
2020-05-30 16:00:33 -0700

    alternate turns and check if cell has been occupied

--- resources/script.js
+++ resources/script.js
@@ -2,8 +2,23 @@ console.log('it works');
 
 var gridItem = document.querySelector('.grid-item')
 
+var player = [0,1];
+var playerColors = ['red', 'green'];
+var playerTurn = player[0];
+
+
+
+//add button
 window.addEventListener('click', function(){
   if (event.target.matches('.click-grid .grid-item')) {
-    event.target.style.background = 'yellow';
+    if (event.target.style.background === '') {
+      event.target.style.background = playerColors[playerTurn];
+      changeTurn();
+
+    }
   }
 });
+
+function changeTurn() {
+  playerTurn === player[0] ? playerTurn = player[1] : playerTurn = player[0];
+}
