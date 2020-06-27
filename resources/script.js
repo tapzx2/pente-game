6e0fcfeec3797cd3bc7ac973475c5fa10c243c2c
Nicholas Owen Tapia <tapia.nicholas@gmail.com>
2020-06-27 15:32:03 -0700

    fully functionally

--- resources/script.js
+++ resources/script.js
@@ -1,5 +1,4 @@
-/*debug capture check*/
-/*5 in a row tests break if checking at edge of the board */
+/* add playing at the bottom and left / right sizes game tests*/
 
 //MODEL
 //cordinates go row, column
@@ -36,15 +35,11 @@ function init () {
   capturePattern = [playerTurn, oppositePlayerTurn, oppositePlayerTurn, playerTurn];
   playingGame = true;
   playerColors = ['rgb(234, 228, 174)', 'rgb(189, 235, 255)'];
+  document.querySelector('.message-box p').textContent = 'Player Turn';
 };
 
 //VIEW
 //on click pass click coordinate location to JS
-
-document.querySelector('.play').addEventListener('click', function(){
-  document.querySelector('.start-screen').style.display = 'none';
-  document.querySelector('.game-frame').style.display = 'block';
-})
 
 function showGrid() {
   for (let i = 0; i<10; i++){
@@ -101,6 +96,14 @@ function onClick(click){
     //add check for capture win
     showGrid();
     updatePlayerTurn();
+    showWinMsg();
+  }
+}
+
+// show win message
+function showWinMsg(){
+  if (playingGame === false){
+    document.querySelector('.message-box p').textContent = 'Wins!';
   }
 }
 
@@ -147,6 +150,8 @@ function checkCaptureDirection(click, rowVec, colVec){
     if (
       click[0] + (rowVec * i) < 0 ||
       click[1] + (colVec * i) < 0 ||
+      click[0] + (rowVec * i) > 9 ||
+      click[1] + (colVec * i) > 9 ||
       (rowVec === 0 && colVec === 0)
     ) {
       //console.log('does not exist on grid, returning false, ending this direction\n')
@@ -174,7 +179,6 @@ function checkfor5win(click) {
     checkHorizontal(click) ||
     checkDiagULDR(click) ||
     checkDiagURDL(click)) {
-    console.log('win!')
     playingGame = false;
   }
 }
@@ -209,6 +213,8 @@ function checkDownLeft(click){
   while(
     click[0] + rowDirection >= 0 &&
     click[1] + colDirection >= 0 &&
+    click[0] + rowDirection <= 9 &&
+    click[1] + colDirection <= 9 &&
     grid[click[0] + rowDirection][click[1] + colDirection] === playerTurn) 
     {
     matchingDownLeft += 1;
@@ -231,6 +237,8 @@ function checkUpLeft(click){
   while(
     click[0] + rowDirection >= 0 &&
     click[1] + colDirection >= 0 &&
+    click[0] + rowDirection <= 9 &&
+    click[1] + colDirection <= 9 &&
     grid[click[0] + rowDirection][click[1] + colDirection] === playerTurn) 
     {
     matchingUpLeft += 1;
@@ -248,6 +256,8 @@ function checkDownRight(click){
   while(
     click[0] + rowDirection >= 0 &&
     click[1] + colDirection >= 0 &&
+    click[0] + rowDirection <= 9 &&
+    click[1] + colDirection <= 9 &&
     grid[click[0] + rowDirection][click[1] + colDirection] === playerTurn) 
     {
     matchingDownRight += 1;
@@ -313,8 +323,9 @@ function checkUp(click){
 function checkDown(click){
   var matchingDown = 0;
   var direction = 1;
+  console.log(click[0] + direction);
   while(
-    click[0] + direction >= 0 &&
+    click[0] + direction <= 9 &&
     grid[click[0] + direction][click[1]] === playerTurn)
     {
     matchingDown += 1;
@@ -326,9 +337,23 @@ function checkDown(click){
 //update model functions
 
 function updatePlayerTurn(){
-  playerTurn === players[0] ? playerTurn = players[1] : playerTurn = players[0];
-  oppositePlayerTurn === players[1] ? oppositePlayerTurn = players[0] : oppositePlayerTurn = players[1];
-  capturePattern = [playerTurn, oppositePlayerTurn, oppositePlayerTurn, playerTurn];
+  if (playingGame === true){
+    playerTurn === players[0] ? playerTurn = players[1] : playerTurn = players[0];
+    oppositePlayerTurn === players[1] ? oppositePlayerTurn = players[0] : oppositePlayerTurn = players[1];
+    capturePattern = [playerTurn, oppositePlayerTurn, oppositePlayerTurn, playerTurn];
+    
+    //change message-box div color
+    console.log('playerTurn:' + playerTurn)
+    console.log('playerColors[0]: ' + playerColors[0]);
+    console.log('playerColors[1]: ' + playerColors[1]);
+  
+    if (playerTurn === players[0]){
+      document.querySelector('.game-screen .first').style.backgroundColor = playerColors[0]
+    }
+    if (playerTurn === players[1]){
+      document.querySelector('.game-screen .first').style.backgroundColor = playerColors[1]
+    }
+  }
 }
 
 function updateBoard(click){
@@ -339,8 +364,68 @@ function makeRecord(click) {
   record.push(click);
 }
 
+
+/* ----------------------- */
+/* START SCREEN */
+/* ----------------------- */
+document.querySelector('.start-screen .play').addEventListener('click', function(){
+  document.querySelector('.start-screen').style.display = 'none';
+  document.querySelector('.game-screen').style.display = 'flex';
+})
+
+document.querySelector('.start-screen .rules').addEventListener('click', function(){
+  document.querySelector('.start-screen').style.display = 'none';
+  document.querySelector('.rules-screen').style.display = 'flex';
+})
+
+/* ----------------------- */
+/* RULES SCREEN */
+/* ----------------------- */
+
+document.querySelector('.back').addEventListener('click', function(){
+  document.querySelector('.rules-screen').style.display = 'none';
+  document.querySelector('.start-screen').style.display = 'flex';
+})
+
+document.querySelector('.captures').addEventListener('click', function(){
+  document.querySelector('.rules-screen').style.display = 'none';
+  document.querySelector('.capture-screen').style.display = 'flex';
+})
+
+/* ----------------------- */
+/* CAPTURE SCREEN */
+/* ----------------------- */
+document.querySelector('.capture-screen .play').addEventListener('click', function(){
+  document.querySelector('.capture-screen').style.display = 'none';
+  document.querySelector('.game-screen').style.display = 'flex';
+})
+
+document.querySelector('.capture-screen .rules').addEventListener('click', function(){
+  document.querySelector('.capture-screen').style.display = 'none';
+  document.querySelector('.rules-screen').style.display = 'flex';
+})
+
+/* ----------------------- */
+/* GAME SCREEN */
+/* ----------------------- */
+
+/* message box color update is in function updatePlayerTurn*/
+
+/* bottom menu */
+document.querySelector('.game-screen .rules').addEventListener('click', function(){
+  document.querySelector('.game-screen').style.display = 'none';
+  document.querySelector('.rules-screen').style.display = 'flex';
+})
+
+document.querySelector('.game-screen .play').addEventListener('click', function(){
+  init();
+  showGrid();
+});
+
+
 /*
-//GAME TESTs
+
+//GAME TESTS
 
 //end game functionality test
 init();
@@ -479,241 +564,4 @@ for (let i = 0; i<gameClicks.length;i++){
 
 
 
-*/
-
-/*
-//CONTROLLER
-
-function onClick(click, board, currentPlayer){
-  console.log('player turn: ' + currentPlayer)
-  console.log('click location: ' + click);
-  if (checkifEmpty(click, board)) {
-    console.log('location empty: true')
-    console.log('updating value...new board:')
-    updateValue(click, board, currentPlayer);
-    console.log(board);
-    //update view
-    console.log('checking for win...')
-    checkfor5win(click, board, currentPlayer);
-    console.log('check completed');
-    // check for capture
-        // If capture remove 2 middle pieces from model
-        // Send change to view
-    // check for capture win
-      // if true
-        // apply end of game view state
-        // stop taking inputs
-    
-    console.log('updating currentPlayer...')
-    currentPlayer === players[0] ? currentPlayer = players[1] : currentPlayer = players[0];
-    console.log(currentPlayer)
-  }
-}
-
-function checkfor5win(click, board, currentPlayer) {
-  if (
-    checkVertical(click, board, currentPlayer) //||
-    //checkHorizontal(click, board, currentPlayer) ||
-    //checkDiagLR(click, board, currentPlayer) ||
-    //checkDiagRL(click, board, currentPlayer)
-  ) { console.log('in a row win vertical')
-      // apply end of game view state
-      // stop taking inputs
-  }
-}
-  
-function checkVertical(click, board, currentPlayer) {
-  return 1 + checkUp(click, board, currentPlayer) + checkDown(click, board, currentPlayer) >= 5;
-}
-  
-function checkUp(click, board, currentPlayer){
-  var matchingUp = 0;
-  var direction = -1;
-  while(board[click[0] + direction][click[1]] === currentPlayer){
-    matchingUp += 1;
-    direction += direction;
-  }
-  return matchingUp
-}
-
-function checkDown(click, board, currentPlayer){
-  var matchingDown = 0;
-  var direction = 1;
-  while(board[click[0] + direction][click[1]] === currentPlayer){
-    matchingDown += 1;
-    direction += direction;
-  }
-  return matchingDown
-}
-
-function checkifEmpty(click, board){
-  if (board[click[0]][click[1]] === 0){
-    return true
-  } else {
-    return false
-  }
-}
-
-function updateValue(click, board, currentPlayer) {
-  board[click[0]][click[1]] = currentPlayer;
-}
-  
-
-
-//GAME TEST
-
-var gameClicks = [
-  [1,1], [1,2],
-  [2,1], [2,2],
-  [3,1], [3,2],
-  [4,1], [4,2],
-  [5,2], [5,2]
-]
-
-function vertWinTest(gameClicks, board, currentPlayer, players){
-  
-  for (let i = 0; i<gameClicks.length;i++){
-    onClick(gameClicks[i], board, currentPlayer, players)
-  }
-}
-
-
-
-//vertWinTest(gameClicks, theBoard, ourCurrentPlayer, players)
-
-*/
-
-/*
-
-var clickGrid = document.querySelectorAll('.click-grid .grid-item');
-var gridSize = clickGrid.length**.5;
-var playerColors = ['rgb(234, 228, 174)', 'rgb(189, 235, 255)'];
-var currentColor = playerColors[0];
-var captureCount = {};
-var playing;
-
-init();
-window.addEventListener('click', function(){
-  //if the clicked spaces is free, update color, check for capture, and check for win states
-  if (event.target.matches('.click-grid .grid-item') && event.target.style.background === '' && playing === true) {
-      event.target.style.background = currentColor;
-      console.log(currentColor);
-      checkForWin(currentColor);
-      //change player turn
-      currentColor === playerColors[0] ? currentColor = playerColors[1] : currentColor = playerColors[0];
-  }
-});
-document.querySelector(".play-again").addEventListener('click', init);
-
-function init() {
-  //sets playing to true, clears all colors, clears capture count, sets player 0 turn, removes play-again button
-  playing = true;
-  var playingBoard = document.querySelectorAll('.click-grid .grid-item');
-  //clear all colors from playingBoard
-  for (let i = 0; i<playingBoard.length; i++) {
-    playingBoard[i].style.background = "";
-  }
-  captureCount[playerColors[0]] = 0;
-  captureCount[playerColors[1]] = 0;
-  currentColor = playerColors[0];
-  document.querySelector(".play-again").style.display = "none";
-
-}
-
-function checkForWin(currentColor){
-  //count row down, then column in... NOT x, y
-  //defines 5 in a row pattern and capturePattern
-  //checks for pattern and win states
-
-  var fiveLongPattern = [currentColor, currentColor, currentColor, currentColor, currentColor];
-  var oppositeColor;
-  currentColor === playerColors[0] ? oppositeColor = playerColors[1] : oppositeColor = playerColors[0];
-  var capturePattern = [currentColor, oppositeColor, oppositeColor, currentColor];
-
-  //starting point coordinates as array
-  var startLoc = event.target.dataset.coordinates.split('-');
-
-  //itterate around starting location coordinate, create 9 point grid
-  var checkGrid = [];
-  for (let i = -1; i<2; i++){
-    for (let j = -1; j<2; j++){
-      checkGrid.push([(parseInt(startLoc[0]) + i), (parseInt(startLoc[1]) + j)])
-    }
-  }
-
-  checkforPattern(checkGrid, fiveLongPattern, startLoc);
-  checkforPattern(checkGrid, capturePattern, startLoc);
-}
-
-function checkforPattern(checkGrid, pattern, startLoc){
-  //in the built check grid, checks for supplied pattern, starting at startLoc
-  //two win conditions are coded in for 5 in a row and 5 captures
-  console.log('\n')
-  console.log(`checking for ${pattern}, starting at: ` + startLoc);
-  //var startLoc = checkGrid.filter(function(coordinate, index){return index === 4;})
-  var checkGridwoCenter = checkGrid.filter(function(coordinate, index){return index !== 4;})
-  //itterate through check grid
-  for (let i = 0; i<8; i++){
-    var testLoc = [checkGridwoCenter[i][0], checkGridwoCenter[i][1]];
-    var vector = [testLoc[0]-parseInt(startLoc[0]), testLoc[1]-parseInt(startLoc[1])];
-    var testLocAsString = `${testLoc[0]}-${testLoc[1]}`
-    var testLocColor = getTestLocColor(testLocAsString);
-    var inarow = 1;
-    for (let j = 1; j<pattern.length; j++){
-      //console.log('testining location: ' + testLoc) //console.log(pattern[j]); //console.log(testLocColor);
-      //check for pattern itteration to testLocColor match
-      if (pattern[j] === testLocColor) {
-        console.log('adding to in a row')
-        inarow++
-        console.log(inarow)
-        //check for 5 in a row win state and end game if true
-        if (pattern.length === 5 && inarow === 5) {
-          alert('winner winner chicken dinner by 5 in a row!');
-          playing = false;
-          document.querySelector(".play-again").style.display = "block";
-        //check for capture pattern and if true update capture count
-        } else if (pattern.length === 4 && inarow === 4) {
-          alert('capture');
-          var remove1 = [testLoc[0] - vector[0], testLoc[1] - vector[1]];
-          console.log('remove1 var: ' + remove1)
-          var remove1AsString = `${remove1[0]}-${remove1[1]}`
-          document.querySelector(`.click-grid [data-coordinates="${remove1AsString}"]`).style.backgroundColor = '';
-          var remove2 = [testLoc[0] - vector[0]*2, testLoc[1] - vector[1]*2];
-          console.log('remove2 var: ' + remove2)
-          var remove2AsString = `${remove2[0]}-${remove2[1]}`
-          document.querySelector(`.click-grid [data-coordinates="${remove2AsString}"]`).style.backgroundColor = '';
-          //update capture captureCount
-          captureCount[pattern[0]] += 1;
-          console.log(captureCount);
-          //check for captureCount winstate
-          if (captureCount[pattern[0]] === 5){
-            alert('winner winner chicken dinner by capture!');
-            document.querySelector(".play-again").style.display = "block";
-            playing = false;
-          }
-          //update test location along vector and get new color at new location
-        } else {
-          console.log('updating to new test loc along vector')
-          testLoc = [testLoc[0] + vector[0], testLoc[1] + vector[1]];
-          console.log('test loc along vector is now: ' + testLoc)
-          //get new color at location
-          testLocAsString = `${testLoc[0]}-${testLoc[1]}`
-          testLocColor = getTestLocColor(testLocAsString);
-        }
-        //if no color match, stop the itteration
-      } else { break; }
-    }
-  }
-}
-
-function getTestLocColor(testLocAsString) {
-  //returns color if available, otherwise, returns empty string
-  //exists because selecting style of a non existant element thows an error and breaks the program
-  var output = document.querySelector(`.click-grid [data-coordinates="${testLocAsString}"]`)
-  if (output !== null){
-    return document.querySelector(`.click-grid [data-coordinates="${testLocAsString}"]`).style.backgroundColor;
-  } else {
-    return "";
-  }
-}
 */
